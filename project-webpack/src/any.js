9e26af9a8992c0d9db450ac87e38e24dd34ee5bd
AlexanderBortsov <bortsov78saltek@gmail.com>
2021-11-16 15:26:34 +0200

    save project-webpack

---
+++ project-webpack/src/any.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+// import './index.css';
+import './style.scss';
+
+setTimeout(()=> {
+    $('body').css({
+        height: '100vh',
+        margin: 0,
+        padding: 0,
+        background: "yellow"
+    })
+}, 10000)
